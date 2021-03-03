c62af40c1f544af7e838709076ce3f218ba449d6
Denys Daniielian <67520699+d3n1x1@users.noreply.github.com>
2021-03-03 09:26:12 +0200

    fix function

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const lightboxImage = document.querySelector('.lightbox__image');
 const lightboxOverlay = document.querySelector('.lightbox__overlay');
 const btnEl = document.querySelector('button[data-action="close-lightbox"]');
 
+let currentIndex;
+
 const cardsMarkup = createCardGallerMarkup(data);
 
 function createCardGallerMarkup(arrGallery) {
   return arrGallery
-    .map(({ preview, original, description }) => {
+    .map(({ preview, original, description }, i) => {
       return `
     <li class="gallery__item">
     <a
@@ -21,6 +23,7 @@ function createCardGallerMarkup(arrGallery) {
     class="gallery__image"
     src="${preview}"
     data-source="${original}"
+    data-index=${i}
     alt="${description}"
     />
     </a>
@@ -34,38 +37,68 @@ galleryRef.insertAdjacentHTML('beforeend', cardsMarkup);
 
 function openModal(e) {
   e.preventDefault();
+  currentIndex = e.target.dataset.index;
 
   if (!e.target.classList.contains('gallery__image')) {
     return;
   }
   lightboxEl.classList.add('is-open');
-  const currentImgUrl = e.target.dataset.source;
-  const currentImgAlt = e.target.alt;
+  let currentImgUrl = e.target.dataset.source;
+  let currentImgAlt = e.target.alt;
   lightboxImage.src = currentImgUrl;
   lightboxImage.alt = currentImgAlt;
+  document.addEventListener('keydown', closeModalOnKey);
+  document.addEventListener('keydown', onArrowPress);
 }
 
-function closeModalOnBtn(e) {
+function closeModalOnBtn() {
   lightboxEl.classList.remove('is-open');
   lightboxImage.src = '';
   lightboxImage.alt = '';
+  document.removeEventListener('keydown', closeModalOnKey);
 }
 
 function closeModalOnOverlay(e) {
   if (e.target.classList.contains('lightbox__image')) return;
-  lightboxEl.classList.remove('is-open');
-  lightboxImage.src = '';
-  lightboxImage.alt = '';
+  closeModalOnBtn();
 }
 
 function closeModalOnKey(e) {
   if (e.key !== 'Escape') return;
-  lightboxEl.classList.remove('is-open');
-  lightboxImage.src = '';
-  lightboxImage.alt = '';
+  closeModalOnBtn();
 }
 
 galleryRef.addEventListener('click', openModal);
 btnEl.addEventListener('click', closeModalOnBtn);
 lightboxOverlay.addEventListener('click', closeModalOnOverlay);
-document.addEventListener('keydown', closeModalOnKey);
+
+function forward() {
+  lightboxImage.src = data[currentIndex + 1].original;
+  currentIndex++;
+}
+function back() {
+  lightboxImage.src = data[currentIndex - 1].original;
+  currentIndex--;
+}
+
+function onArrowPress(e) {
+  if (e.key === 'ArrowLeft') {
+    back();
+  }
+  if (e.key === 'ArrowRight') {
+    forward();
+  }
+}
+
+//   function frw(){
+//   if (current < kolvo) {
+//   current++;
+//   document.images['r'].src=tumb[current-1].src;
+//   }
+//   }
+//   function bck(){
+//   if (current > 1) {
+//   current--;
+//   document.images['r'].src=tumb[current-1].src;
+//   }
+//   }
